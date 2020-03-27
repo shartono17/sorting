3144fa5d88482084ba7e8220c075a9ceb38f59ec
Sabrina Hartono <shartono21@cmc.edu>
2020-03-26 22:16:14 -0700

    v2.4 indentation typos

--- sorting.py
+++ sorting.py
@@ -41,6 +41,7 @@ def cmp_last_digit(a,b):
 
 
 def _merged(xs, ys, cmp=cmp_standard):
+
     list1 = len(xs)
     list2 = len(ys)
 
@@ -55,16 +56,16 @@ def _merged(xs, ys, cmp=cmp_standard):
             sorted_list.append(ys[j])
             j += 1
 
-        if i == list1 and j == list2:
-            return sorted_list
-        elif i == list1:
-            for x in range(j,list2):
-                sortedL.append(ys[x])
-            return sorted_list
-        elif j == list2:
-            for x in range(i, list1):
-                sorted_list.append(xs[x])
-            return sorted_list
+    if i == list1 and j == list2:
+        return sorted_list
+    elif i == list1:
+        for x in range(j,list2):
+            sortedL.append(ys[x])
+        return sorted_list
+    elif j == list2:
+        for x in range(i, list1):
+            sorted_list.append(xs[x])
+        return sorted_list
     
     '''
     Assumes that both xs and ys are sorted,
